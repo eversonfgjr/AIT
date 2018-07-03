b7af9a9fd731bec030cf11b2901290816cd1497a
everson.junior-tqi <eversonfjr@gmail.com>
2018-07-03 10:56:46 -0300

    Alteracao do banco de dados para postgreSQL

---
+++ src/main/java/br/ufu/ppgco/Configuracao.java
@@ -0,0 +1,52 @@
+package br.ufu.ppgco;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@ComponentScan()
+public class Configuracao {
+
+	/*@Bean
+	public DataSource datasource() {
+		DriverManagerDataSource datasource = new DriverManagerDataSource();
+		datasource.setDriverClassName("com.mysql.jdbc.Driver");
+		datasource.setUrl("jdbc:mysql://localhost:3306/ait");
+		datasource.setUsername("root");
+		datasource.setPassword("0408751");
+		return datasource;
+	}*/
+	
+//	@Autowired
+//	private static ApplicationContext app;
+	
+	@Bean
+	public DataSource datasource() {
+		DriverManagerDataSource datasource = new DriverManagerDataSource();
+		datasource.setDriverClassName("org.postgresql.Driver");
+		datasource.setUrl("jdbc:postgresql://localhost:5432/ait-db");
+		datasource.setUsername("aituser");
+		datasource.setPassword("iniciar");
+		return datasource;
+	}
+	
+		
+	public static void main(String[] args) {
+		
+//		for (String bean: app.getBeanDefinitionNames()) {
+//			System.out.println(bean);
+//		}
+//		
+		
+		SpringApplication.run(Configuracao.class, args);
+	}
+}
--- src/main/java/br/ufu/ppgco/controllers/CursosController.java
+++ src/main/java/br/ufu/ppgco/controllers/CursosController.java
@@ -3,16 +3,24 @@ package br.ufu.ppgco.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.ufu.ppgco.entities.Curso;
 import br.ufu.ppgco.repository.CursoRepository;
 
-@Controller
+@RestController
 public class CursosController {
 
 	@Autowired
 	public CursoRepository cursoRepository;
+	
+	@GetMapping("/")
+	public String ola() {
+		return "ola";
+	}
 	
 	
 	@RequestMapping("listaCursos")
