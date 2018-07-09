df0334ccd56ea096ebed87ec182d50e0bbe14137
everson.junior-tqi <eversonfjr@gmail.com>
2018-07-09 11:08:07 -0300

    Insercao de cursos

--- src/main/java/br/ufu/ppgco/controllers/CursosController.java
+++ src/main/java/br/ufu/ppgco/controllers/CursosController.java
@@ -3,7 +3,10 @@ package br.ufu.ppgco.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.ufu.ppgco.entities.Curso;
@@ -21,12 +24,30 @@ public class CursosController {
 		return "ola";
 	}
 	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 	
-	@RequestMapping("listaCursos")
+	@RequestMapping("/curso")
+	public String curso() {
+		return "curso";
+	}
+	
+	
+	@RequestMapping("/cursos/list")
 	public String listaCursos(Model model) {
 		Iterable<Curso> it =  cursoRepository.findAll();
 		model.addAttribute("cursos", it);
 		return null;
+	}
+	
+	@RequestMapping(value="teste", method = RequestMethod.POST)
+	public  String salvar(@RequestParam("nome") String nome, Model model) {
+		Curso curso = new Curso();
+		curso.setNome(nome);
+		cursoRepository.save(curso);
+		return "curso";
 	}
 	
 }
--- src/main/java/br/ufu/ppgco/security/SpringWebSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package br.ufu.ppgco.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringWebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
